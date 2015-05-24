3bc28747d91e74c6633c1cb506c45fe75082d5b4
Lakshmi Kollapudi <lakshmikollapudi@Lakshmis-MacBook-Air.local>
2015-05-24 17:02:46 -0400

    after merge

--- MailApp/ViewController.m
+++ MailApp/ViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"master");
     NSLog(@"in branch11, desnt exists in master");
+    NSLog(@"after merger");
 }
 
 - (void)didReceiveMemoryWarning {
